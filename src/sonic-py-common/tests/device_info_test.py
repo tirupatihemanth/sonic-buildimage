55747189ec3f48685b0a0574345af73c1e301847
Saikrishna Arcot <sarcot@microsoft.com>
2025-11-23 11:45:17 -0800

    Revert "Support for single ASIC VOQ Fixed-Systems (#24163)"
    This reverts commit bb0612a10b9733947d1b6602dc26844eb1129dea.

--- src/sonic-py-common/tests/device_info_test.py
+++ src/sonic-py-common/tests/device_info_test.py
@@ -120,41 +120,29 @@ class TestDeviceInfo(object):
             # Assert the file was read only once
             open_mocked.assert_called_once_with(device_info.SONIC_VERSION_YAML_PATH)
 
-    @mock.patch("sonic_py_common.device_info.is_chassis_config_absent")
     @mock.patch("sonic_py_common.device_info.get_platform_info")
     @mock.patch("sonic_py_common.device_info.is_disaggregated_chassis")
-    def test_is_chassis(self, mock_is_disaggregated_chassis, mock_platform_info, mock_is_chassis_config_absent):
+    def test_is_chassis(self, mock_is_disaggregated_chassis, mock_platform_info):
         mock_platform_info.return_value = {"switch_type": "npu"}
         mock_is_disaggregated_chassis.return_value = False
-        mock_is_chassis_config_absent.return_value = False
         assert device_info.is_chassis() == False
         assert device_info.is_voq_chassis() == False
         assert device_info.is_packet_chassis() == False
 
         mock_platform_info.return_value = {"switch_type": "voq"}
         mock_is_disaggregated_chassis.return_value = False
-        mock_is_chassis_config_absent.return_value = False
         assert device_info.is_voq_chassis() == True
         assert device_info.is_packet_chassis() == False
         assert device_info.is_chassis() == True
 
         mock_platform_info.return_value = {"switch_type": "voq"}
         mock_is_disaggregated_chassis.return_value = True
-        mock_is_chassis_config_absent.return_value = False
         assert device_info.is_voq_chassis() == True
-        assert device_info.is_packet_chassis() == False
-        assert device_info.is_chassis() == False
-
-        mock_platform_info.return_value = {"switch_type": "voq"}
-        mock_is_disaggregated_chassis.return_value = False
-        mock_is_chassis_config_absent.return_value = True
-        assert device_info.is_voq_chassis() == False
         assert device_info.is_packet_chassis() == False
         assert device_info.is_chassis() == False
 
         mock_platform_info.return_value = {"switch_type": "chassis-packet"}
         mock_is_disaggregated_chassis.return_value = False
-        mock_is_chassis_config_absent.return_value = False
         assert device_info.is_voq_chassis() == False
         assert device_info.is_packet_chassis() == True
         assert device_info.is_chassis() == True
@@ -162,7 +150,6 @@ class TestDeviceInfo(object):
         mock_platform_info.return_value = {"switch_type": "dummy-sup",
                                            "asic_type": "vs"}
         mock_is_disaggregated_chassis.return_value = False
-        mock_is_chassis_config_absent.return_value = False
         assert device_info.is_voq_chassis() == False
         assert device_info.is_packet_chassis() == False
         assert device_info.is_virtual_chassis() == True
@@ -170,7 +157,6 @@ class TestDeviceInfo(object):
 
         mock_platform_info.return_value = {}
         mock_is_disaggregated_chassis.return_value = False
-        mock_is_chassis_config_absent.return_value = False
         assert device_info.is_voq_chassis() == False
         assert device_info.is_packet_chassis() == False
         assert device_info.is_chassis() == False
