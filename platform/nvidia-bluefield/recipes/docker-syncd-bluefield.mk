d39d5464a0d98e63b74a324e01ac837c435ce946
Hemanth Kumar Tirupati <htirupati@nvidia.com>
2026-03-13 17:04:25 +0000

    [CONTAINER_TRIXIE] upgrade syncd nvidia-bluefield to trixie
    Signed-off-by: Hemanth Kumar Tirupati <htirupati@nvidia.com>

--- platform/nvidia-bluefield/recipes/docker-syncd-bluefield.mk
+++ platform/nvidia-bluefield/recipes/docker-syncd-bluefield.mk
@@ -16,7 +16,7 @@
 #
 
 DOCKER_SYNCD_PLATFORM_CODE = bluefield
-include $(PLATFORM_PATH)/../template/docker-syncd-bookworm.mk
+include $(PLATFORM_PATH)/../template/docker-syncd-trixie.mk
 
 $(DOCKER_SYNCD_BASE)_DEPENDS += $(SYNCD) $(MFT) $(SDN_APPL) $(DPU_SAI) $(MLNX_IPROUTE2)
 
